06b79a730b87bd3af2a1dd7a0e0ea8ecd8fde5de
Priyank <priyank74@rediffmail.com>
2023-03-27 20:19:33 +0530

    Example of Control Statement

---
+++ ControlStmnt.java
@@ -0,0 +1,13 @@
+public class ControlStmnt {
+
+    public static void main(String[] args) {
+        int x = 14;
+        if(x%2==0){
+            System.out.println("The Integer is an even number");
+        } else if (x%2!=0) {
+            System.out.println("The number is an odd number");
+        }
+
+
+    }
+}
